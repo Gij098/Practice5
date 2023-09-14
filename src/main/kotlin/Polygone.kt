d8794c1a86344934b61d34cbf263b1fb81fb68fc
Gij098 <122697331+Gij098@users.noreply.github.com>
2023-09-14 22:39:59 +0300

    Update Polygone.kt

--- src/main/kotlin/Polygone.kt
+++ src/main/kotlin/Polygone.kt
@@ -1,5 +1,3 @@
-import java.lang.ProcessHandle.Info
-
 class Polygon(startPoint: Point, endPoint: Point, vararg var _vertices: Point) : Line(startPoint, endPoint) {
 
     // Метод для перемещения по оси OX
